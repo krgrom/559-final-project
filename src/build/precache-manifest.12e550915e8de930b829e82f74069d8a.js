17ffe2a7cbee68707e932d1f9201a04f514e892f
krgrom <karina.o.gonzalez@rutgers.edu>
2020-05-08 00:48:23 -0400

    First commit

---
+++ src/build/precache-manifest.12e550915e8de930b829e82f74069d8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f538447b5edd49c224a",
+    "url": "/static/css/main.d0b91bf2.chunk.css"
+  },
+  {
+    "revision": "5f538447b5edd49c224a",
+    "url": "/static/js/main.a167a107.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e3411b17d3915443b45c",
+    "url": "/static/js/2.4a7f71bf.chunk.js"
+  },
+  {
+    "revision": "d28bf8b6e831cfcee356975f61cbbd7a",
+    "url": "/index.html"
+  }
+];
